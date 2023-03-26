56a54bc28d3a27d62ebea5f7552bac1120456373
Anderson Meireles <anderson_meireles@id.uff.br>
2023-03-26 13:24:58 -0300

    removing useless api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   scope path: 'api' do
-    api_guard_routes for: 'users'
+    api_guard_routes for: 'users', only: [:authentication]
   end
   constraints subdomain: 'api' do
-    api_guard_routes for: 'users'
+    api_guard_routes for: 'users', only: [:authentication]
   end
   namespace :api do
     resources :alerts, only: [:index, :create]
